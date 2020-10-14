a1509d1fb73b82a01af637db7534a9c1d1dd447b
Ihor Markevych <vukpud@gmail.com>
2020-10-13 20:33:04 -0400

    hour interval selection added

--- main.py
+++ main.py
@@ -28,26 +28,32 @@ selector_pick = alt.selection_interval(empty='none', encodings=['x', 'y'])
 selector_drop = alt.selection_interval(empty='none', encodings=['x', 'y'])
 
 taxi_map_pickup_chart = alt.Chart(taxi_map_pickup_dropoff).mark_circle(size=2).encode(
-    alt.X('dropoff_latitude', scale=alt.Scale(zero=False)),
-    alt.Y('dropoff_longitude', scale=alt.Scale(zero=False)),
+    alt.X('dropoff_latitude', scale=alt.Scale(zero=False, domain=[40.5, 40.9])),
+    alt.Y('dropoff_longitude', scale=alt.Scale(zero=False, domain=[-74.2, -73.7])),
 #     alt.Color('time', title='Number of dropoffs'),
     color = alt.condition(selector_drop, alt.value('blue'), alt.value('grey')),
-    tooltip=[alt.Tooltip('fare_amount', title='Number of dropoffs')]
+#    tooltip=[alt.Tooltip('fare_amount', title='Number of dropoffs')]
 ).add_selection(selector_pick)
 
 taxi_map_dropoff_chart = alt.Chart(taxi_map_pickup_dropoff).mark_circle(size=2).encode(
-    alt.X('pickup_latitude', scale=alt.Scale(zero=False)),
-    alt.Y('pickup_longitude', scale=alt.Scale(zero=False)),
+    alt.X('pickup_latitude', scale=alt.Scale(zero=False, domain=[40.5, 40.9])),
+    alt.Y('pickup_longitude', scale=alt.Scale(zero=False, domain=[-74.2, -73.7])),
 #     alt.Color('time', title='Number of dropoffs'),
     color = alt.condition(selector_pick, alt.value('red'), alt.value('grey')),
-    tooltip=[alt.Tooltip('fare_amount', title='Number of dropoffs')]
+#    tooltip = [alt.Tooltip('fare_amount', title='Number of dropoffs')]
 ).add_selection(selector_drop)
 
-slider = alt.binding_range(min=1, max=5, step=1)
-hour = alt.selection_single(name="Slider", fields=['hour'], bind=slider)
+selector_hour = alt.selection_interval(empty='all', encodings=['x'])
 
-st.write((taxi_map_pickup_chart | taxi_map_dropoff_chart).add_selection(
-    hour
-).transform_filter(
-    hour
-))
+hour_chart = alt.Chart(taxi.groupby("hour").count().dropoff_latitude.reset_index()).mark_bar().encode(
+    alt.X("hour"),
+    alt.Y("dropoff_latitude", title="Number of rides"),
+    color = alt.condition(selector_hour, alt.value('blue'), alt.value('grey')),
+).properties(
+    width=900,
+    height=100
+).add_selection(selector_hour)
+
+((taxi_map_pickup_chart | taxi_map_dropoff_chart).transform_filter(
+    selector_hour
+) & hour_chart)
