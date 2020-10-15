adbe4ddc8a17a2363ae39b94e66dd60213ec00d5
Ihor Markevych <vukpud@gmail.com>
2020-10-15 18:17:46 -0400

    opacity added to maps

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ taxi_map_dropoff_chart = alt.Chart(taxi_map_pickup_dropoff, title="Taxi Dropoffs
 #     alt.Color('time', title='Number of dropoffs'),
     color = alt.condition(selector_drop, alt.value('blue'), alt.value('grey')),
 #    tooltip=[alt.Tooltip('fare_amount', title='Number of dropoffs')]
+    opacity=alt.condition(selector_drop, alt.value(1), alt.value(0.1))
 ).add_selection(selector_pick)
 
 taxi_map_pickup_chart = alt.Chart(taxi_map_pickup_dropoff, title="Taxi Pickups").mark_square(size=5).encode(
@@ -37,6 +38,7 @@ taxi_map_pickup_chart = alt.Chart(taxi_map_pickup_dropoff, title="Taxi Pickups")
 #     alt.Color('time', title='Number of dropoffs'),
     color = alt.condition(selector_pick, alt.value('red'), alt.value('grey')),
 #    tooltip = [alt.Tooltip('fare_amount', title='Number of dropoffs')]
+    opacity=alt.condition(selector_pick, alt.value(1), alt.value(0.1))
 ).add_selection(selector_drop)
 
 selector_hour = alt.selection_interval(empty='all', encodings=['x'])
@@ -44,7 +46,8 @@ selector_hour = alt.selection_interval(empty='all', encodings=['x'])
 hour_chart = alt.Chart(taxi.groupby("hour").count().dropoff_latitude.reset_index()).mark_bar().encode(
     alt.X("hour"),
     alt.Y("dropoff_latitude", title="Number of rides"),
-    color = alt.condition(selector_hour, alt.value('blue'), alt.value('grey')),
+    color=alt.condition(selector_hour, alt.value('blue'), alt.value('grey')),
+    opacity=alt.condition(selector_hour, alt.value(1), alt.value(0.5))
 ).properties(
     width=900,
     height=100
